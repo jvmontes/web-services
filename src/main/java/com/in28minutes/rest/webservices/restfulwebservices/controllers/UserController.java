a4827e38352003ed3e060737bd6368694717811e
Jorge Viramontes <jviramontes@solstice-consulting.com>
2019-07-12 19:14:58 -0500

    Added Input Validation & created a Bad Request response. Added Delete by ID to the User Service Added Delete User to the User Controller Added HATEOAS to the response. Added Unit Tests for UserController using a Mock UserService.

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controllers/UserController.java
@@ -4,12 +4,18 @@ import com.in28minutes.rest.webservices.restfulwebservices.exception.UserNotFoun
 import com.in28minutes.rest.webservices.restfulwebservices.user.User;
 import com.in28minutes.rest.webservices.restfulwebservices.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UserController {
@@ -17,22 +23,32 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    public UserController(UserService userService) {
+        this.service = userService;
+    }
+
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if (user == null) {
             throw new UserNotFoundException("id-" + id);
         }
-        return user;
+
+        // HATEOAS
+        // "all-users", SERVER_PATH + "/users"
+        EntityModel<User> model = new EntityModel<User>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+        return model;
     }
 
     @PostMapping("/users")
-    public ResponseEntity createUser(@RequestBody User user) {
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder.
@@ -42,6 +58,14 @@ public class UserController {
                 toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null)
+            throw new UserNotFoundException("id-" + id);
     }
 
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,15 +2,20 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 public class User {
 
+
     private Integer id;
 
+    @Size(min=2, message = "Name should have at least two characters")
     private String name;
 
+    @Past
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
